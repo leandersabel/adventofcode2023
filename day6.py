ce79f9a15b4d91f37601fb6579d638430552b186
Leander <leander.sabel@gmail.com>
2023-12-06 14:49:34 +0100

    Optimized solution for day 6
    Implemented binary search based solution for day 6 (part 2)

--- day6.py
+++ day6.py
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2023/day/
+# https://adventofcode.com/2023/day/6
 
 import math
 
@@ -30,10 +30,30 @@ class DaySolution(Solution):
         distance = int(''.join([d for d in self.parsed_input[1]]))
         first_win = None
 
-        for t in range(0, time + 1):
-            if predict(t, time) > distance:
-                first_win = t
-                break
+        approach = 'binary-search'  # Options are 'naive' or 'binary-search'
+
+        if approach == 'naive':
+            for t in range(0, time + 1):
+                if predict(t, time) > distance:
+                    first_win = t
+                    break
+
+        elif approach == 'binary-search':
+            c = math.ceil(time / 2)
+            window = [0, time]
+
+            while first_win is None:
+                result = predict(c, time)
+
+                if window[1] - window[0] == 0:
+                    first_win = c + 1
+
+                if result > distance:
+                    window = [window[0], c - 1]
+                    c = window[1] - math.ceil((window[1] - window[0]) / 2)
+                else:
+                    window = [c + 1, window[1]]
+                    c = window[0] + math.ceil((window[1] - window[0]) / 2)
 
         return time - 1 - (first_win - 1) * 2
 
